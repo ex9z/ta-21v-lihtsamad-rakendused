8f8a75b935b2edbff590a811560dfaab4ec5875b
) <dansteel53@gmail.com>
2022-03-24 10:27:54 +0200

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,23 @@
+
+operation = input("+ , - , * , /\n")
+
+if operation == "+":
+    num1 = int(input("num1 : "))
+    num2 = int(input("num2 : "))
+    print(num1+num2)
+
+if operation == "-":
+    num1 = int(input("num1 : "))
+    num2 = int(input("num2 : "))
+    print(num1-num2)
+
+if operation == "*":
+    num1 = int(input("num1 : "))
+    num2 = int(input("num2 : "))
+    print(num1*num2)
+
+if operation == "/":
+    num1 = int(input("num1 : "))
+    num2 = int(input("num2 : "))
+    print(num1/num2)
+

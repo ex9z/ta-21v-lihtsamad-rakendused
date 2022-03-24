5d3eec47fa624b55d8edb9f950f509a4f80000ad
) <dansteel53@gmail.com>
2022-03-24 11:07:09 +0200

    Update 2_tictactoe.py

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -1,3 +1,46 @@
-print(". . x")
-print(". o .")
-print(". . .")
+next_pos = input("position : ")
+
+if next_pos == "1":
+    print("x . .")
+    print(". . .")
+    print(". . .")
+
+if next_pos == "2":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_pos == "3":
+    print(". . x")
+    print(". . .")
+    print(". . .")
+
+if next_pos == "4":
+    print(". . .")
+    print("x . .")
+    print(". . .")
+
+if next_pos == "5":
+    print(". . .")
+    print(". x .")
+    print(". . .")
+
+if next_pos == "6":
+    print(". . .")
+    print(". . x")
+    print(". . .")
+
+if next_pos == "7":
+    print(". . .")
+    print(". . .")
+    print("x . .")
+
+if next_pos == "8":
+    print(". . .")
+    print(". . .")
+    print(". x .")
+
+if next_pos == "9":
+    print(". . .")
+    print(". . .")
+    print(". . x")

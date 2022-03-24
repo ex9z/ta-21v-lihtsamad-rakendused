3aa2315e78b0813f0ea5b24d1885d543afd67fa7
) <dansteel53@gmail.com>
2022-03-24 11:26:54 +0200

    booooriiiinnnnnnngggg

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -1,46 +1,143 @@
-next_pos = input("position : ")
+next_pos = input("x position : ")
 
 if next_pos == "1":
     print("x . .")
     print(". . .")
     print(". . .")
 
-if next_pos == "2":
-    print(". x .")
-    print(". . .")
-    print(". . .")
+    next_pos = input("o position : ")
 
-if next_pos == "3":
-    print(". . x")
-    print(". . .")
-    print(". . .")
+    if next_pos == "1":
+        print("cant do it")
+    
+    elif next_pos == "2":
+        print("x o .")
+        print(". . .")
+        print(". . .")
 
-if next_pos == "4":
-    print(". . .")
-    print("x . .")
-    print(". . .")
+        next_pos = input("x position : ")
 
-if next_pos == "5":
-    print(". . .")
-    print(". x .")
-    print(". . .")
+        if next_pos == "1":
+            print("cant do it")
+        
+        elif next_pos == "2":
+            print("cant do it")
+        
+        elif next_pos == "3":
+            print("x o x")
+            print(". . .")
+            print(". . .")
+        
+        elif next_pos == "4":
+            print("x o .")
+            print("x . .")
+            print(". . .")
+        
+        elif next_pos == "5":
+            print("x o .")
+            print(". x .")
+            print(". . .")
+        
+        elif next_pos == "5":
+            print("x o .")
+            print(". . x")
+            print(". . .")
 
-if next_pos == "6":
-    print(". . .")
-    print(". . x")
-    print(". . .")
+    
+    elif next_pos == "3":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+    
+    elif next_pos == "4":
+        print("x . .")
+        print("o . .")
+        print(". . .")
 
-if next_pos == "7":
-    print(". . .")
-    print(". . .")
-    print("x . .")
+    elif next_pos == "5":
+        print("x . .")
+        print(". o .")
+        print(". . .")
 
-if next_pos == "8":
-    print(". . .")
-    print(". . .")
-    print(". x .")
+    elif next_pos == "6":
+        print("x . o")
+        print(". . o")
+        print(". . .")
 
-if next_pos == "9":
-    print(". . .")
-    print(". . .")
-    print(". . x")
+    elif next_pos == "7":
+        print("x . .")
+        print(". . .")
+        print("o . .")
+
+    elif next_pos == "8":
+        print("x . .")
+        print(". . .")
+        print(". o .")
+
+    elif next_pos == "9":
+        print("x . .")
+        print(". . .")
+        print(". . o")
+
+
+    elif next_pos == "2":
+        print(". x .")
+        print(". . .")
+        print(". . .")
+
+        next_pos = input("o position : ")
+
+        if next_pos == "1":
+            print("o x .")
+            print(". . .")
+            print(". . .")
+        
+        elif next_pos == "2":
+            print("cant do the move")
+        
+        elif next_pos == "3":
+            print(". x o")
+            print(". . .")
+            print(". . .")
+        
+        elif next_pos =="4":
+            print(". x .")
+            print("o . .")
+            print(". . .")
+        
+
+
+    elif next_pos == "3":
+        print(". . x")
+        print(". . .")
+        print(". . .")
+
+    elif next_pos == "4":
+        print(". . .")
+        print("x . .")
+        print(". . .")
+
+    elif next_pos == "5":
+        print(". . .")
+        print(". x .")
+        print(". . .")
+
+    elif next_pos == "6":
+        print(". . .")
+        print(". . x")
+        print(". . .")
+
+    elif next_pos == "7":
+        print(". . .")
+        print(". . .")
+        print("x . .")
+
+    elif next_pos == "8":
+        print(". . .")
+        print(". . .")
+        print(". x .")
+
+    elif next_pos == "9":
+        print(". . .")
+        print(". . .")
+        print(". . x")
